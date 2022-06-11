710953bb78354a0943c54c0c31c6c5a55eb85534
NahrulK <khayattullah73@gmail.com>
2022-06-11 10:36:30 +0800

    Prepare for About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar/Navbar";
+import About from "./Pages/About/About";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    
+    <div className="app">
+      <Navbar />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
