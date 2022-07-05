04f8650e4052be18dc041215c1dc539c3d9f52f3
dion fananie <dionfananie@gmail.com>
2022-07-05 09:18:19 +0700

    add eslintrc

--- src/App.js
+++ src/App.js
@@ -1,83 +1,82 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
-import About from "./Pages/About/About";
-import Login from "./Pages/Login/Login";
-import Home from "./Pages/Home/Home";
-import Cyberpage from "./Pages/Flashcard/Cyberpage";
-import Profile from "./Pages/Profile/Profile";
-import { auth } from "./firebase";
-import { useEffect } from "react";
-import { useStateValue } from "./StateProvider";
-import Sexualpage from "./Pages/Flashcard/Sexualpage";
-import Physicalpage from "./Pages/Flashcard/Physicalpage";
-import Verbalpage from "./Pages/Flashcard/Verbalpage";
-
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import Cyberpage from './Pages/Flashcard/Cyberpage';
+import Profile from './Pages/Profile/Profile';
+import { auth } from './firebase';
+import { useEffect } from 'react';
+import { useStateValue } from './StateProvider';
+import Sexualpage from './Pages/Flashcard/Sexualpage';
+import Physicalpage from './Pages/Flashcard/Physicalpage';
+import Verbalpage from './Pages/Flashcard/Verbalpage';
 
 function App() {
-  const [{}, dispatch] = useStateValue();
+    const [{}, dispatch] = useStateValue(); // kalo error dieslint, silahkan di benerin jd const [, dispatch] = useStateValue()
 
-  useEffect(() => {
-    // will only run once when the app component loads...
+    useEffect(() => {
+        // will only run once when the app component loads...
 
-    auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
+        auth.onAuthStateChanged((authUser) => {
+            console.log('THE USER IS >>> ', authUser);
 
-      if (authUser) {
-        // the user just logged in / the user was logged in
+            if (authUser) {
+                // the user just logged in / the user was logged in
 
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
+                dispatch({
+                    type: 'SET_USER',
+                    user: authUser
+                });
+            } else {
+                // the user is logged out
+                dispatch({
+                    type: 'SET_USER',
+                    user: null
+                });
+            }
         });
-      } else {
-        // the user is logged out
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
-      }
-    });
-  }, []);
+    }, []);
 
-  return (
-    <body>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-            <Route path="/Cybercard">
-              <Cyberpage />
-            </Route>
-            <Route path="/Sexualcard">
-              <Sexualpage />
-            </Route>
-            <Route path="/Physicalcard">
-              <Physicalpage />
-            </Route>
-            <Route path="/Verbalcard">
-              <Verbalpage />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </body>
-  );
+    return (
+        <body>
+            <Router>
+                <div className="app">
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path="/profile">
+                            <Profile />
+                        </Route>
+                        <Route path="/Cybercard">
+                            <Cyberpage />
+                        </Route>
+                        <Route path="/Sexualcard">
+                            <Sexualpage />
+                        </Route>
+                        <Route path="/Physicalcard">
+                            <Physicalpage />
+                        </Route>
+                        <Route path="/Verbalcard">
+                            <Verbalpage />
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </div>
+            </Router>
+        </body>
+    );
 }
 
 export default App;
--- src/reducer.js
+++ src/reducer.js
@@ -1,47 +1,47 @@
 export const initialState = {
-  user: null,
-  fav: [],
+    user: null,
+    fav: []
 };
 
 const reducer = (state, action) => {
-  // console.log(action);
-  switch (action.type) {
-    case "ADD_TO_FAV":
-      return {
-        ...state,
-        fav: [...state.fav, action.item],
-      };
+    // console.log(action);
+    switch (action.type) {
+        case 'ADD_TO_FAV':
+            return {
+                ...state,
+                fav: [...state.fav, action.item]
+            };
 
-    case "EMPTY_FAV":
-      return {
-        ...state,
-        fav: [],
-      };
+        case 'EMPTY_FAV':
+            return {
+                ...state,
+                fav: []
+            };
 
-    case "REMOVE_FAV":
-      const index = state.fav.findIndex((favItem) => favItem.id === action.id);
-      let newFav = [...state.fav];
+        case 'REMOVE_FAV':
+            const index = state.fav.findIndex((favItem) => favItem.id === action.id); // kalo error di eslintrc, silahkan dibenerin, kalo gbsa pake switch pake if else jg gpp
+            const newFav = [...state.fav];
 
-      if (index >= 0) {
-        newFav.splice(index, 1);
-      } else {
-        console.warn(`Cant remove fav (id: ${action.id}) as its not in list!`);
-      }
+            if (index >= 0) {
+                newFav.splice(index, 1);
+            } else {
+                console.warn(`Cant remove fav (id: ${action.id}) as its not in list!`);
+            }
 
-      return {
-        ...state,
-        fav: newFav,
-      };
+            return {
+                ...state,
+                fav: newFav
+            };
 
-    case "SET_USER":
-      return {
-        ...state,
-        user: action.user,
-      };
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 export default reducer;
