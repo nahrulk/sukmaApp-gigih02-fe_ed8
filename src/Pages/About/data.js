88b5fc068ec6fa3f22f27c9ba95494f4b2d80cfa
unknown <shafasalsabilapribadi@gmail.com>
2022-07-07 22:24:25 +0700

    improvement shafa

---
+++ src/Pages/About/data.js
@@ -0,0 +1,31 @@
+import Erryck from "./erryck.jpg"
+import Lutfiah from "./lutfiah.jpg"
+import Nahrul from "./nahrul.jpg"
+import Shafa from "./shafa.jpg"
+
+export const dataCard = [
+  {
+    id: 'KM_G2FE3068',
+    title: 'Erryck Norrys',
+    position: 'Project Manager, Front-end Engineer',
+    img: Erryck
+  },
+  {
+    id: 'KM_G2FE3027',
+    title: 'Nahrul Khayatullah',
+    position: 'Back-end Engineer, Front-end Engineer',
+    img: Nahrul
+  },
+  {
+  id: ' KM_G2FE6240',
+  title: 'Luthfiah Ananda P',
+  position: 'Content Writer, Front-end Engineer',
+  img: Lutfiah
+  },
+  {
+    id: 'KM_G2FE4040',
+    title: 'Shafa Salsabila P',
+    position: 'UI/UX Designer, Front-end Engineer',
+    img: Shafa
+  }
+]
