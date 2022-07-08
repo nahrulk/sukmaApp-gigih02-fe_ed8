10dff6585921f6d58e412fc8344d52e94d4f3e9a
NahrulK <khayattullah73@gmail.com>
2022-07-08 15:32:37 +0800

    Reinstalling eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,11 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['prettier'],
-  plugins: ['prettier'],
+  extends: ['eslint:recommended',
+  'plugin:react/recommended', 'prettier/recommended'],
   rules: {
     'generator-star-spacing': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }], 
   },
 }
